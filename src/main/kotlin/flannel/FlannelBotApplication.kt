381dd0f1895beeaa7d569d658e84fe8940e314e5
Gwendolyn Wahl <wahl.gwen@gmail.com>
2019-05-15 23:21:43 -0700

    Add procfile, application plugin, environment variable config loading for simplicity

--- src/main/kotlin/flannel/FlannelBotApplication.kt
+++ src/main/kotlin/flannel/FlannelBotApplication.kt
@@ -4,10 +4,11 @@ package flannel
 import discord4j.core.DiscordClientBuilder
 import discord4j.core.event.domain.lifecycle.ReadyEvent
 import discord4j.core.event.domain.message.MessageCreateEvent
+import java.lang.IllegalArgumentException
 
 fun main(args : Array<String>) {
-    // TODO obviously don't hard code token, load in a config file or something?
-    val token = if (args.isNotEmpty()) args[0]  else ""
+    val token = System.getenv("FLANNEL_BOT_TOKEN") ?: ""
+    if (token.isBlank()) throw IllegalArgumentException("No discord bot token configured.")
     val client = DiscordClientBuilder(token).build()
     client.eventDispatcher.on(ReadyEvent::class.java).subscribe {
             ready -> println("Logged in as ${ready.self.username}" )
